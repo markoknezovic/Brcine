db0539af3dc0f4d7eb6109a0b24be229eb1b5b57
markoknezovic <58472152+markoknezovic@users.noreply.github.com>
2021-03-08 09:19:16 +0100

    Add files via upload

---
+++ Brcine/Brcine/Brcine.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brcine
+{
+    class Brcine
+    {
+        string poda1, poda2, poda4, dodatak;
+        int poda3;
+
+
+        public string getPod1(string pod1)
+        {
+            return this.poda1;
+        }
+        public string poda1 { get => poda1; set => poda1 = value; }
+        public string poda2 { get => poda2; set => poda2 = value; }
+        public string poda4 { get => poda4; set => poda4 = value; }
+        public string dodatak { get => dodatak; set => dodatak = value }
+
+        public int poda3 { get => poda3; set => poda3 = value; }
+
+        public Brcine()
+            public Brcine(string poda1, string poda2, int poda3, string poda4)
+
+        {
+            this.poda1 = poda1;
+            this.poda2 = poda2;
+            this.poda3 = poda3;
+            this.poda4 = poda4;
+        }
+        public string Ispis()
+        {
+            string ispis = poda1 + "\t" + poda2 + "\t"
+                + Convert.ToString(poda3) + "\t"
+                + poda4 + "\r\n";
+            return ispis;
+        }
+        public override string ToString()
+        {
+            +Convert.ToString(poda3) + "\t\t"
+                + poda4 + "\t\t" + dodatak + "\r\n";
+            return Ispis;
+        }
+    }
+
+}
+
---
+++ Brcine/Brcine/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Brcine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.poda1 = new System.Windows.Forms.TextBox();
+            this.poda2 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.poda4 = new System.Windows.Forms.TextBox();
+            this.poda3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // poda1
+            // 
+            this.poda1.Location = new System.Drawing.Point(30, 47);
+            this.poda1.Multiline = true;
+            this.poda1.Name = "poda1";
+            this.poda1.Size = new System.Drawing.Size(100, 20);
+            this.poda1.TabIndex = 0;
+            // 
+            // poda2
+            // 
+            this.poda2.Location = new System.Drawing.Point(30, 100);
+            this.poda2.Multiline = true;
+            this.poda2.Name = "poda2";
+            this.poda2.Size = new System.Drawing.Size(100, 20);
+            this.poda2.TabIndex = 1;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.ContextMenuStrip1_Opening);
+            // 
+            // poda4
+            // 
+            this.poda4.Location = new System.Drawing.Point(30, 202);
+            this.poda4.Multiline = true;
+            this.poda4.Name = "poda4";
+            this.poda4.Size = new System.Drawing.Size(100, 20);
+            this.poda4.TabIndex = 3;
+            // 
+            // poda3
+            // 
+            this.poda3.Location = new System.Drawing.Point(30, 148);
+            this.poda3.Multiline = true;
+            this.poda3.Name = "poda3";
+            this.poda3.Size = new System.Drawing.Size(100, 20);
+            this.poda3.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Podatak 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Podatak 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Podatak 3";
+            this.label3.Click += new System.EventHandler(this.Label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 186);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Podatak 4";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(148, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 175);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Unesi";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(331, 47);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(173, 175);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Obradi";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.Location = new System.Drawing.Point(510, 47);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(184, 175);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Ispiši";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(30, 248);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(758, 208);
+            this.richTextBox1.TabIndex = 12;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.RichTextBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.poda3);
+            this.Controls.Add(this.poda4);
+            this.Controls.Add(this.poda2);
+            this.Controls.Add(this.poda1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox poda1;
+        private System.Windows.Forms.TextBox poda2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox poda4;
+        private System.Windows.Forms.TextBox poda3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
